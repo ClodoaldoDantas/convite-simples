f1732171c88b9e8430d5c60a13ddfc5d2687d659
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2025-12-29 22:06:35 -0300

    feat: enhance sign-in and sign-up pages with additional prompts for account creation and login

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,17 @@
+import Link from 'next/link'
 import { SignInForm } from './_components/sign-in-form'
 
 export default function SignInPage() {
-  return <SignInForm />
+  return (
+    <>
+      <SignInForm />
+
+      <p className="text-center mt-4 text-zinc-500">
+        Ainda não tem uma conta?{' '}
+        <Link className="text-blue-600 hover:underline" href="/sign-up">
+          Cadastre-se
+        </Link>
+      </p>
+    </>
+  )
 }
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,17 @@
+import Link from 'next/link'
 import { SignUpForm } from './_components/sign-up-form'
 
 export default function SignUpPage() {
-  return <SignUpForm />
+  return (
+    <>
+      <SignUpForm />
+
+      <p className="text-center mt-4 text-zinc-500">
+        Já tem uma conta?{' '}
+        <Link className="text-blue-600 hover:underline" href="/sign-in">
+          Faça login
+        </Link>
+      </p>
+    </>
+  )
 }
